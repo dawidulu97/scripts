d6addcf270d8fd91ef67d11e275e6dbceab900fe
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-10-08 22:34:17 -0700

    Use default logo if no custom logo is inputted

--- action/index.ts
+++ action/index.ts
@@ -23,7 +23,7 @@ void (async () => {
 
   const bodyLines = body.split('\n')
 
-  console.log(bodyLines)
+  console.log('Issue body lines:', bodyLines)
 
   const boardName = bodyLines[2].trim().toLowerCase()
 
@@ -31,42 +31,46 @@ void (async () => {
 
   await fs.writeFile(process.env.GITHUB_OUTPUT as string, `boardName=${boardName}`, 'utf8')
 
-  const logoMarkdown = bodyLines[6]
+  const logoMarkdown = bodyLines[6].trim()
 
-  const imageUrlRegex = /!\[.*\]\((.*)\)/g
-  const match = (imageUrlRegex.exec(logoMarkdown))?.[1] ?? never('Invalid markdown logo')
+  if (logoMarkdown !== '') {
+    const imageUrlRegex = /!\[.*\]\((.*)\)/g
+    const match = (imageUrlRegex.exec(logoMarkdown))?.[1] ?? never('Invalid markdown logo')
 
-  console.log(`Parsed logo URL: ${match}`)
+    console.log(`Parsed logo URL: ${match}`)
 
-  const r = await fetch(match)
-  const contentType = r.headers.get('Content-Type')
+    const r = await fetch(match)
+    const contentType = r.headers.get('Content-Type')
 
-  const blob = await r.blob()
-  const logoBuffer = Buffer.from(await blob.arrayBuffer())
-  const maxSize = 250
+    const blob = await r.blob()
+    const logoBuffer = Buffer.from(await blob.arrayBuffer())
+    const maxSize = 250
 
-  let scaledLogo: Jimp
-  if (contentType === 'image/svg+xml') {
-    console.log('Detected SVG logo. Creating a 250x250 PNG logo from the SVG logo')
-    const pngBuffer = await svgToPng(logoBuffer, {
-      width: maxSize,
-      height: maxSize
-    })
-    scaledLogo = await Jimp.create(pngBuffer)
-    // logo.write(path.join(__dirname, 'test.png'))
-  } else {
-    const logo = await Jimp.create(logoBuffer)
-    console.log(`Original logo: ${logo.getWidth()}x${logo.getHeight()} (${logo.getExtension()})`)
-    if (logo.getWidth() > maxSize || logo.getHeight() > maxSize) {
-      console.log(`Resizing logo to a maximum of ${maxSize}x${maxSize}`)
-      scaledLogo = logo.scaleToFit(maxSize, maxSize)
-      console.log(`New logo size: ${scaledLogo.getWidth()}x${scaledLogo.getHeight()}`)
+    let scaledLogo: Jimp
+    if (contentType === 'image/svg+xml') {
+      console.log('Detected SVG logo. Creating a 250x250 PNG logo from the SVG logo')
+      const pngBuffer = await svgToPng(logoBuffer, {
+        width: maxSize,
+        height: maxSize
+      })
+      scaledLogo = await Jimp.create(pngBuffer)
+      // logo.write(path.join(__dirname, 'test.png'))
     } else {
-      console.log(`Logo is within ${maxSize}x${maxSize}. Not resizing`)
-      scaledLogo = logo
+      const logo = await Jimp.create(logoBuffer)
+      console.log(`Original logo: ${logo.getWidth()}x${logo.getHeight()} (${logo.getExtension()})`)
+      if (logo.getWidth() > maxSize || logo.getHeight() > maxSize) {
+        console.log(`Resizing logo to a maximum of ${maxSize}x${maxSize}`)
+        scaledLogo = logo.scaleToFit(maxSize, maxSize)
+        console.log(`New logo size: ${scaledLogo.getWidth()}x${scaledLogo.getHeight()}`)
+      } else {
+        console.log(`Logo is within ${maxSize}x${maxSize}. Not resizing`)
+        scaledLogo = logo
+      }
     }
-  }
 
-  console.log('Saving logo to Documentation/coreboot_logo.bmp in coreboot folder', path.join(__dirname, '../coreboot/Documentation/coreboot_logo.bmp'))
-  await scaledLogo.writeAsync(path.join(__dirname, '../coreboot/Documentation/coreboot_logo.bmp'))
+    console.log('Saving logo to Documentation/coreboot_logo.bmp in coreboot folder', path.join(__dirname, '../coreboot/Documentation/coreboot_logo.bmp'))
+    await scaledLogo.writeAsync(path.join(__dirname, '../coreboot/Documentation/coreboot_logo.bmp'))
+  } else {
+    console.log('No custom logo inputted. Default logo will be used')
+  }
 })()
