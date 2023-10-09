b5d4e68ca251f19d021ec304b277c2290a6ba3a1
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-10-09 11:10:30 -0700

    Upload artifact with sha1 and flash script

--- action-sh/zip-rom.sh
+++ action-sh/zip-rom.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-TMP=$(mktemp)
-echo $TMP
-echo "Hi"
-pwd
+TMP=$(mktemp -d)
+cp coreboot/build/coreboot.rom $TMP/$BUILD_FILE_NAME.rom
+cp action-sh/flash-full-rom.sh $TMP
+cp action-sh/flashrom $TMP
+TMP2=$(mktemp -d)
+cd $TMP
+echo $BUILD_FILE_NAME >BUILD_FILE_NAME
+sha1sum $BUILD_FILE_NAME.rom >$BUILD_FILE_NAME.rom.sha1
+tar -czvf $TMP2/$BUILD_FILE_NAME.tar.gz .
+echo artifactPath=$TMP2/$BUILD_FILE_NAME.tar.gz >>$GITHUB_OUTPUT
