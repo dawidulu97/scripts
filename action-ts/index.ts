78fa9a71f9fa0d7ce682ddbb375e8d9e7f7af2e8
LaptopStore <dawidulu97@gmail.com>
2025-10-01 20:09:38 +0300

    Update index.ts

--- action-ts/index.ts
+++ action-ts/index.ts
@@ -31,14 +31,14 @@ void (async () => {
 
     const blob = await r.blob()
     const logoBuffer = Buffer.from(await blob.arrayBuffer())
-    const maxSize = 650
+    const maxSize = 630
 
     let scaledLogo: Jimp
     if (contentType === 'image/svg+xml') {
       console.log('Detected SVG logo. Creating a 500x500 PNG logo from the SVG logo')
       const pngBuffer = await svgToPng(logoBuffer, {
         width: maxSize,
-        height: maxSize
+        height: 130
       })
       scaledLogo = await Jimp.create(pngBuffer)
       // logo.write(path.join(__dirname, 'test.png'))
